574f9ee5c0cf37f851fddc52c7c0e6b1310ee02d
chivehao <chivehao@ikaros.run>
2025-02-19 03:55:22 +0800

    添加resty

---
+++ api/dandanplay/search.go
@@ -0,0 +1,9 @@
+package dandanplay
+
+import "run/ikaros/danmuku/config"
+
+func SearchEpisodesWithKeyword(confg config.Config, anime string, episode string) {
+	// var appid  = confg.Dandanplay.AppId
+	// var appSecret = confg.Dandanplay.AppSecret
+
+}
